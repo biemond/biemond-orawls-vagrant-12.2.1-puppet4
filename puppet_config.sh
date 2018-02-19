fbdfac17fa9e0353250a13b3516e6860688d06df
danielmenezesbr <danielmenezes@gmail.com>
2018-02-19 18:15:39 -0300

    removed unnecessary command

--- puppet_config.sh
+++ puppet_config.sh
@@ -2,4 +2,3 @@
 ln -sf /vagrant/puppet/hiera.yaml /etc/puppetlabs/code/hiera.yaml;
 rm -rf /etc/puppetlabs/code/modules;
 ln -sf /vagrant/puppet/environments/development/modules /etc/puppetlabs/code/modules;
-ln -sf /vagrant/software /
